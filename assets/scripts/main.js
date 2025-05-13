b41b0f10d3927021543897f43d12fd1618421325
kalkulator413 <kalkin413@gmail.com>
2025-05-13 13:50:04 -0700

    finish expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,10 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	let recipeList = localStorage.getItem('recipes');
+	if (!recipeList)
+		return [];
+	return JSON.parse(recipeList);
 }
 
 /**
@@ -35,10 +39,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	let mainEl = document.querySelector("main");
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	for (let i = 0; i < recipes.length; i++) {
+		let newEl = document.createElement('recipe-card');
+		newEl.data = recipes[i];
+		mainEl.appendChild(newEl);
+	}
 }
 
 /**
